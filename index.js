fba03e27906020ea14f8f3010adb087bcbd92dc9
Logan Kuzyk <logan@kuzyk.net>
2020-02-29 18:27:17 -0800

    Basic logic

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const {
   poweredByHandler
 } = require('./handlers.js')
 
-// For deployment to Heroku, the port needs to be set using ENV, so
-// we check for the port number in process.env
 app.set('port', (process.env.PORT || 9001))
 
 app.enable('verbose errors')
@@ -19,13 +17,20 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(poweredByHandler)
 
-// --- SNAKE LOGIC GOES BELOW THIS LINE ---
+function willCollide () {
+  if (
+    request.pos['x'] < 0
+    || request.pos['x'] < 0
+    || request.post['x'] > board['width'] - 1
+    || request.post['y'] > board['height'] - 1) {
+      return true
+    } else {
+      return false
+    }
+}
 
-// Handle POST request to '/start'
 app.post('/start', (request, response) => {
-  // NOTE: Do something here to start the game
 
-  // Response data
   const data = {
     color: '#DFFF00',
   }
@@ -33,29 +38,23 @@ app.post('/start', (request, response) => {
   return response.json(data)
 })
 
-// Handle POST request to '/move'
 app.post('/move', (request, response) => {
-  // NOTE: Do something here to generate your move
-
-  // Response data
-  const data = {
-    move: 'up', // one of: ['up','down','left','right']
+  const data = {}
+  if (willCollide) {
+    data = {move: 'left'}
+  } else {
+    data = {move: 'right'}
   }
-
   return response.json(data)
 })
 
 app.post('/end', (request, response) => {
-  // NOTE: Any cleanup when a game is complete.
   return response.json({})
 })
 
 app.post('/ping', (request, response) => {
-  // Used for checking if this snake is still alive.
   return response.json({});
 })
-
-// --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
 app.use('*', fallbackHandler)
 app.use(notFoundHandler)
