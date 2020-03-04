bdd290feb502b3a7a3a66e3366603a993e182509
Logan Kuzyk <logan@kuzyk.net>
2020-03-03 16:19:28 -0800

    Reset, change color to white

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const {
   poweredByHandler
 } = require('./handlers.js')
 
+// For deployment to Heroku, the port needs to be set using ENV, so
+// we check for the port number in process.env
 app.set('port', (process.env.PORT || 9001))
 
 app.enable('verbose errors')
@@ -17,46 +19,43 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(poweredByHandler)
 
-function willCollide () {
-  if (
-    request.pos['x'] < 0
-    || request.pos['x'] < 0
-    || request.pos['x'] > board['width'] - 1
-    || request.pos['y'] > board['height'] - 1) {
-      return true
-    } else {
-      return false
-    }
-}
+// --- SNAKE LOGIC GOES BELOW THIS LINE ---
 
+// Handle POST request to '/start'
 app.post('/start', (request, response) => {
+  // NOTE: Do something here to start the game
 
+  // Response data
   const data = {
-    color: "#736CCB",
-    headType: "beluga",
-    tailType: "curled"
+    color: '#FFFFFF',
   }
 
   return response.json(data)
 })
 
+// Handle POST request to '/move'
 app.post('/move', (request, response) => {
-  const data = {move='left'}
-  // if (willCollide) {
-  //   data = {move: 'left'}
-  // } else {
-  //   data = {move: 'right'}
-  // }
+  // NOTE: Do something here to generate your move
+
+  // Response data
+  const data = {
+    move: 'up', // one of: ['up','down','left','right']
+  }
+
   return response.json(data)
 })
 
 app.post('/end', (request, response) => {
+  // NOTE: Any cleanup when a game is complete.
   return response.json({})
 })
 
 app.post('/ping', (request, response) => {
+  // Used for checking if this snake is still alive.
   return response.json({});
 })
+
+// --- SNAKE LOGIC GOES ABOVE THIS LINE ---
 
 app.use('*', fallbackHandler)
 app.use(notFoundHandler)
