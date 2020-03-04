64588c2239b08b75a47e5eae682ed7c88e93c38a
Logan Kuzyk <logan@kuzyk.net>
2020-03-04 13:09:45 -0800

    Change layout of functions

--- snake.js
+++ snake.js
@@ -1,32 +1,32 @@
-var request = {}
+let request = {}
 
-function possibleMoves () {
-    possibilities = ['up', 'right', 'down', 'left']
-    let x = request.you.body[0].x
-    let y = request.you.body[0].y
-
-    // Checks surround blocks to see if occupied by snake bodies.
-    for (let snake of request.board.snakes) {
-        for (let i = 0; i < snake.body.length; i++) {
-            if (snake.body[i].x == x + 1) {
+// Returns possible moves for given snake.
+function snakeOptions (snake) {
+    let possibilities = ['up', 'right', 'left', 'down']
+    let x = snake.body[0].x
+    let y = snake.body[0].y
+    
+    for (let other of request.board.snakes) {
+        for (let i = 0; i < other.body.length; i++) {
+            if (other.body[i].x == x + 1) {
                 let rem = possibilities.indexOf('right')
                 if (rem >= 0) {
                     possibilities.splice(rem, 1)
                 }
             }
-            if (snake.body[i].x == x - 1) {
+            if (other.body[i].x == x - 1) {
                 let rem = possibilities.indexOf('left')
                 if (rem >= 0) {
                     possibilities.splice(rem, 1)
                 }
             }
-            if (snake.body[i].y == y + 1) {
+            if (other.body[i].y == y + 1) {
                 let rem = possibilities.indexOf('up')
                 if (rem >= 0) {
                     possibilities.splice(rem, 1)
                 }
             }
-            if (snake.body[i].y == y - 1) {
+            if (other.body[i].y == y - 1) {
                 let rem = possibilities.indexOf('down')
                 if (rem >= 0) {
                     possibilities.splice(rem, 1)
@@ -34,7 +34,7 @@ function possibleMoves () {
             }
         }
     }
-    
+
     if (x == 0) {
         let rem = possibilities.indexOf('left')
         if (rem >= 0) {
@@ -62,35 +62,41 @@ function possibleMoves () {
     return possibilities
 }
 
-function decide () {
-    let options = possibleMoves()
+// function heatMap () {
+//     for (let snake of request.board.snakes) {
+        
+//     }
+// }
+
+// Returns a move going towards a given tile.
+// TODO: Add weights for wanted moves, return highest weighted one.
+function moveTowards ({x, y}) {
+    let possible = snakeOptions(request.you)
     let want = []
-
-    for (let nug of request.board.food) {
-        if (request.you.body[0].x < nug.x) {
-            if (want.indexOf('right') < 0) {
-                want.push('right')
-            }
-        } else if (request.you.body[0].x > nug.x) {
-            if (want.indexOf('left') < 0) {
-                want.push('left')
-            }
+    
+    if (request.you.body[0].x < x) {
+        if (want.indexOf('right') < 0) {
+            want.push('right')
         }
+    } else if (request.you.body[0].x > x) {
+        if (want.indexOf('left') < 0) {
+            want.push('left')
+        }
+    }
 
-        if (request.you.body[0].y < nug.y) {
-            if (want.indexOf('up') < 0) {
-                want.push('up')
-            }
-        } else if (request.you.body[0].y > nug.y){
-            if (want.indexOf('down') < 0) {
-                want.push('down')
-            }
+    if (request.you.body[0].y < nug.y) {
+        if (want.indexOf('up') < 0) {
+            want.push('up')
+        }
+    } else if (request.you.body[0].y > y){
+        if (want.indexOf('down') < 0) {
+            want.push('down')
         }
     }
 
     let move = 'left'
 
-    for (let dir of options) {
+    for (let dir of possible) {
         if (want.indexOf(dir) >= 0) {
             move = dir
         }
@@ -101,6 +107,6 @@ function decide () {
 
 module.exports = function (apiRequest) {
     request = apiRequest
-    let move = decide()
+    let move = moveTowards(request.board.food[0])
     return move
 }
