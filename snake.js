6c9449b86634beca6b345f4b077f321cd8b8ea8e
Logan Kuzyk <logan@kuzyk.net>
2020-03-08 16:35:32 -0700

    Initial move prediction

--- snake.js
+++ snake.js
@@ -1,55 +1,52 @@
 let request = {}
+let requestText = ''
 let mode = ''
+let maxIterations = 3
 
 // Thinking methods are for life-saving moves and cannot be ignored. snakeOptions prevents immediate death, the others prevent death within the next few turns.
 class Thinking {
-    constructor (snake) {
-        this.snake = snake
+    constructor () {
     }
 
-    getSnake = function () {
-        return this.snake
-    }
-    
     // Returns possible moves for given snake.
-    snakeOptions = function (snake) {
+    snakeOptions = function (snake, nextRequest) {
         let possibilities = ['up', 'right', 'left', 'down']
         let x = snake.body[0].x
         let y = snake.body[0].y
 
-        for (let other of request.board.snakes) {
+        for (let other of nextRequest.board.snakes) {
             for (let i = 0; i < other.body.length; i++) {
                 if (other.body[i].x == x + 1 && other.body[i].y == y) {
-                    if (request.board.width > (x + 1) >= 0) {
-                        request.board.possibilities[x + 1][y] = 1
-                    }
+                    // if (nextRequest.board.width > (x + 1) >= 0) {
+                    //     nextRequest.board.possibilities[x + 1][y] = 1
+                    // }
                     let rem = possibilities.indexOf('right')
                     if (rem >= 0) {
                         possibilities.splice(rem, 1)
                     }
                 }
                 if (other.body[i].x == x - 1 && other.body[i].y == y) {
-                    if (request.board.width > (x - 1) >= 0) {
-                        request.board.possibilities[x - 1][y] = 1
-                    }
+                    // if (nextRequest.board.width > (x - 1) >= 0) {
+                    //     nextRequest.board.possibilities[x - 1][y] = 1
+                    // }
                     let rem = possibilities.indexOf('left')
                     if (rem >= 0) {
                         possibilities.splice(rem, 1)
                     }
                 }
                 if (other.body[i].y == y + 1 && other.body[i].x == x) {
-                    if (request.board.width > (y + 1) >= 0) {
-                        request.board.possibilities[x][y + 1] = 1
-                    }
+                    // if (nextRequest.board.width > (y + 1) >= 0) {
+                    //     nextRequest.board.possibilities[x][y + 1] = 1
+                    // }
                     let rem = possibilities.indexOf('down')
                     if (rem >= 0) {
                         possibilities.splice(rem, 1)
                     }
                 }
                 if (other.body[i].y == y - 1 && other.body[i].x == x) {
-                    if (request.board.width > (y - 1) >= 0) {
-                        request.board.possibilities[x][y - 1] = 1
-                    }
+                    // if (nextRequest.board.width > (y - 1) >= 0) {
+                    //     nextRequest.board.possibilities[x][y - 1] = 1
+                    // }
                     let rem = possibilities.indexOf('up')
                     if (rem >= 0) {
                         possibilities.splice(rem, 1)
@@ -63,7 +60,7 @@ class Thinking {
             if (rem >= 0) {
                 possibilities.splice(rem, 1)
             }
-        } else if (x == request.board.width - 1) {
+        } else if (x == nextRequest.board.width - 1) {
             let rem = possibilities.indexOf('right')
             if (rem >= 0) {
                 possibilities.splice(rem, 1)
@@ -75,7 +72,7 @@ class Thinking {
             if (rem >= 0) {
                 possibilities.splice(rem, 1)
             }
-        } else if (y == request.board.height - 1) {
+        } else if (y == nextRequest.board.height - 1) {
             let rem = possibilities.indexOf('down')
             if (rem >= 0) {
                 possibilities.splice(rem, 1)
@@ -85,80 +82,147 @@ class Thinking {
         return possibilities
     }
 
-    avoidTraps = function () {
-        let snake = this.getSnake()
+    simulateHelper = function (apiRequest, move, iterations) {
+        let simRequest = JSON.parse(apiRequest)
+        let snake = simRequest.you
+        let head = snake.body[0]
+        iterations++
+        if (iterations == maxIterations + 1 || this.snakeOptions(snake, simRequest).length == 0) {
+            return 0
+        }
+        for (let other of simRequest.board.snakes) {
+            if (other.body.length == 1) {
+                break
+            }
+            let x = other.body[other.body.length - 1].x
+            let y = other.body[other.body.length - 1].y
+            simRequest.board.possibilities[x][y]--
+            other.body.pop()
+        }
+        
+        // Moves head to given move.
+        snake = simRequest.you
+        if (move == 'right') {
+            snake.body.unshift({x: head.x + 1, y: head.y})
+        } else if (move == 'left') {
+            snake.body.unshift({x: head.x - 1, y: head.y})
+        } else if (move == 'up') {
+            snake.body.unshift({x: head.x, y: head.y - 1})
+        } else if (move == 'down') {
+            snake.body.unshift({x: head.x, y: head.y + 1})
+        }
+
+        // Check if snake moved off the board.
+        if (0 > snake.body[0].x || snake.body[0].x >= simRequest.board.width) {
+            return 0
+        } else if (0 > snake.body[0].y || snake.body[0].y >= simRequest.board.height) {
+            return 0
+        }
+
+        for (let move of this.probabilityFlow(simRequest)) {
+            snake = simRequest.you
+            let newRequest = JSON.stringify(simRequest)
+            // console.log(snake.body[0].x + ',' + snake.body[0].y + ' moving ' + move)
+            if (move == 'right') {
+                return 1 + this.simulateHelper(newRequest, move, iterations)
+            } else if (move == 'left') {
+                return 1 + this.simulateHelper(newRequest, move, iterations)
+            } else if (move == 'up') {
+                return 1 + this.simulateHelper(newRequest, move, iterations)
+            } else if (move == 'down') {
+                return 1 + this.simulateHelper(newRequest, move, iterations)
+            }
+        }
+    }
+
+    // Runs simulations and returns best one, entry point for using simulations.
+    simulate = function () {
+        let result = {right: 0, left: 0, up: 0, down: 0}
+        let final = []
+        for (let move of this.probabilityFlow(request)) {
+            // console.log('simulating ' + move)
+            result[move] = this.simulateHelper(requestText, move, 0)
+            // console.log(result[move])
+        }
+        let max = Math.max(result.right, result.left, result.up, result.down)
+        if (result['right'] == max) {
+            final.push('right')
+        } if (result['left'] == max) {
+            final.push('left')
+        } if (result['up'] == max) {
+            final.push('up')
+        } if (result['down'] == max) {
+            final.push('down')
+        }
+        console.log(result)
+        // console.log(final)
+        return final
     }
 
     // Returns list of moves that are least likely to collide.
-    probabilityFlow = function () {
-        let snake = this.getSnake()
+    // TODO: make HTTP request and snake parameters.
+    probabilityFlow = function (apiRequest) {
+        let snake = apiRequest.you
 
-        for (let other of request.board.snakes) {
+        for (let other of apiRequest.board.snakes) {
             // Ignoring probablity of own snake moving to a tile.
             if (other.body[0].x == snake.body[0].x && other.body[0].y == snake.body[0].y) {
-                break
+                continue
             }
+
             let head = other.body[0]
-            for (let possibility of this.snakeOptions(other)) {
-                let count = this.snakeOptions(other).length
+            for (let possibility of this.snakeOptions(other, apiRequest)) {
+                let count = this.snakeOptions(other, apiRequest).length
                 if (possibility == 'right') {
-                    request.board.possibilities[head.x + 1][head.y] += 1/count
+                    apiRequest.board.possibilities[head.x + 1][head.y] += 1/count
                 } else if (possibility == 'left') {
-                    request.board.possibilities[head.x - 1][head.y] += 1/count
+                    apiRequest.board.possibilities[head.x - 1][head.y] += 1/count
                 } else if (possibility == 'up') {
-                    request.board.possibilities[head.x][head.y - 1] += 1/count
+                    apiRequest.board.possibilities[head.x][head.y - 1] += 1/count
                 } else if (possibility == 'down') {
-                    request.board.possibilities[head.x][head.y + 1] += 1/count
+                    apiRequest.board.possibilities[head.x][head.y + 1] += 1/count
                 }
             }
         }
-        
         let right = 1
         let left = 1
         let up = 1
         let down = 1
-
-        for (let option of this.snakeOptions(snake)) {
+        for (let option of this.snakeOptions(snake, apiRequest)) {
             if (option == 'right') {
-                right = request.board.possibilities[snake.body[0].x + 1][snake.body[0].y]
+                right = apiRequest.board.possibilities[snake.body[0].x + 1][snake.body[0].y]
             } else if (option == 'left') {
-                left = request.board.possibilities[snake.body[0].x - 1][snake.body[0].y]
+                left = apiRequest.board.possibilities[snake.body[0].x - 1][snake.body[0].y]
             } else if (option == 'up') {
-                up = request.board.possibilities[snake.body[0].x][snake.body[0].y - 1]
+                up = apiRequest.board.possibilities[snake.body[0].x][snake.body[0].y - 1]
             } else if (option == 'down') {
-                down = request.board.possibilities[snake.body[0].x][snake.body[0].y + 1]
+                down = apiRequest.board.possibilities[snake.body[0].x][snake.body[0].y + 1]
             }
         }
 
-        console.log('right ' + right)
-        console.log('left ' + left)
-        console.log('up ' + up)
-        console.log('down ' + down)
+        // console.log('right ' + right)
+        // console.log('left ' + left)
+        // console.log('up ' + up)
+        // console.log('down ' + down)
+        
 
         let min = ''
-        let result = []
+        let options = []
         
-        if (right < left) {
-           min = right
-        } else if (left < up) {
-            min = left
-        } else if (up < down) {
-            min = up
-        } else if (down < right) {
-            min = down
-        }
-
+        min = Math.min(right, left, up, down)
         if (right == min) {
-            result.push('right')
+            options.push('right')
         } if (left == min) {
-            result.push('left')
+            options.push('left')
         } if (up == min) {
-            result.push('up')
+            options.push('up')
         } if (down == min) {
-            result.push('down')
-        }
-        console.log('probflow chooses ' + result)
-        return result
+            options.push('down')
+        }        
+        // TODO: make sure probabilities are only being edited once per turn simulation.        
+        
+        // console.log('probflow chooses ' + options)
+        return options
     }
 }
 
@@ -252,24 +316,48 @@ function mood () {
     mode = 'general'
 }
 
+function closestFood () {
+    let x = request.you.body[0].x
+    let y = request.you.body[0].y
+    let minDistance = 100 // TODO: make this unhard coded.
+    let minFood = {}
+
+    for (let food of request.board.food) {
+        let deltax = food.x - x
+        let deltay = food.y - y
+        // console.log(Math.sqrt(deltax**2 + deltay**2))
+        if (Math.sqrt(deltax**2 + deltay**2) < minDistance) {
+            // console.log(food)
+            minFood = food
+        }
+    }
+    // console.log('closest food is ' + minDistance + " away " + minFood.x + " " + minFood.y)
+    
+    if (minDistance == 100) {
+        return request.board.food[0]
+    }
+
+    return minFood
+}
+
 function brain () {
     let think = {left: 0, right: 0, up: 0, down: 0} // Moves that can be made in order to stay alive.
     let feel = {left: 0, right: 0, up: 0, down: 0} // Moves that the snake wants to make depending on strategy.
     let thinking = new Thinking(request.you)
     let feeling = new Feeling()
 
-    for (let move of feeling.moveTowards(request.board.food[0])) {
-        let i = feel[move]
-        if (i >= 0) {
-            feel[move]++
-        } else {
-            feel[move] = 1
-        }
-    }
+    // for (let move of feeling.moveTowards(closestFood())) {
+    //     let i = feel[move]
+    //     if (i >= 0) {
+    //         feel[move]++
+    //     } else {
+    //         feel[move] = 1
+    //     }
+    // }
 
-    for (let move of feeling.snakeDirection(request.you)) {
-        feel[move]++
-    }
+    // for (let move of feeling.snakeDirection(request.you)) {
+    //     feel[move]++
+    // }
 
     // for (let move of thinking.snakeOptions(request.you)) {
     //     let i = think[move]
@@ -280,7 +368,7 @@ function brain () {
     //     }
     // }
 
-    for (let move of thinking.probabilityFlow()) {
+    for (let move of thinking.simulate()) {
         let i = think[move]
         if (i >= 0) {
             think[move]++
@@ -309,31 +397,33 @@ function brain () {
     }
 
     // Finding most popular non suicidal move. (One with the most "votes")
-    let max = ''
-    if (feel.left > feel.right) {
-        max = 'left'
-    } else if (feel.right > feel.up) {
-        max = 'right'
-    } else if (feel.up > feel.down) {
-        max = 'up'
-    } else if (feel.down > feel.left) {
-        max = 'down'
+    let max = Math.max(feel.right, feel.left, feel.up, feel.down)
+    
+    // console.log(think)
+    // console.log(feel)
+
+    for (let move of Object.keys(feel)) {
+        if (feel[move] == max) {
+            return move
+        }
     }
-    console.log(think)
-    console.log(feel)
-    return max
 }
 
 module.exports = function (apiRequest) {
     request = apiRequest
     request.board.possibilities = []
+    console.log(request.you.body[0])
+    console.time('now')
     for (let i = 0; i < request.board.width; i++) {
         request.board.possibilities.push([])
         for (let j = 0; j < request.board.width; j++) {
             request.board.possibilities[i][j] = 0
         }
     }
+    requestText = JSON.stringify(request)
     let message = brain()
+    console.log(request.you.body[0])
     console.log(message)
+    console.timeEnd('now')
     return message
 }
