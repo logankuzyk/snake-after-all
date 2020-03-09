0ce56507796819de00bfbc0b2b482bb85151f4f5
Logan Kuzyk <logan@kuzyk.net>
2020-03-09 13:11:21 -0700

    Typo

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 let request = {}
 let requestText = ''
 let mode = ''
-let maxIterations = 10
+let maxIterations = 100
 
 // Thinking methods are for life-saving moves and cannot be ignored. snakeOptions prevents immediate death, the others prevent death within the next few turns.
 class Thinking {
@@ -76,7 +76,7 @@ class Thinking {
         let head = snake.body[0]
         iterations++
         if (iterations == maxIterations + 1) {
-            return ' ran out of tries'
+            return 0
         }
         for (let other of simRequest.board.snakes) {
             if (other.body.length == 0) {
