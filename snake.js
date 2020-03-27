ea79298c8bc37237af6ab91d4312fa0edbf7b93d
Logan Kuzyk <logan@kuzyk.net>
2020-03-27 12:56:30 -0700

    Fix adding probabilities to bigger snakes

--- snake.js
+++ snake.js
@@ -237,7 +237,7 @@ class Thinking {
     // TODO: make it so that it only adds probability score to outer layer of blocks.
     // simulateHelper is the only other recursive function now, step should be checked compared to simulateHelper's iterations. Therefore iterations should be made global.
     // Doesn't update probability of coordinate itself but the 'free' ones around it.
-    updateProbsHelper = function (coord, lastCoord, apiRequest) {
+    updateProbsHelper = function (coord, lastCoord, apiRequest, size) {
         let moves = [{x: coord.x + 1, y: coord.y}, {x: coord.x - 1, y: coord.y}, {x: coord.x, y: coord.y + 1}, {x: coord.x, y: coord.y - 1}]
         for (let i = 0; i < moves.length; i++) {
             if (moves[i].x == lastCoord.x && moves[i].y == lastCoord.y || moves[i].x >= apiRequest.board.width || moves[i].y >= apiRequest.board.height || moves[i].x < 0 || moves[i].y < 0) {
@@ -251,9 +251,9 @@ class Thinking {
 
         for (let move of moves) {
             // console.log(move)
-            if (mode == 'bigger') {
+            if (size == 'bigger') {
                 apiRequest.board.possibilities[move.x][move.y] += apiRequest.board.possibilities[coord.x][coord.y] * 1/moves.length
-            } else if (mode == 'smaller') {
+            } else if (size == 'smaller') {
                 // apiRequest.board.possibilities[move.x][move.y] -= apiRequest.board.possibilities[coord.x][coord.y] * 1/moves.length
             }
             newStorage.push([move, coord, mode])
