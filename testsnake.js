e2433def07bc2607a775868d946816577dee2c54
Logan Kuzyk <logan@kuzyk.net>
2020-03-29 08:50:38 -0700

    Attack only when biggest snake on board - avoid dying first

--- testsnake.js
+++ testsnake.js
@@ -9,7 +9,7 @@ let request = {
       "height": 11,
       "width": 11,
       "food": [
-        {'x': 3, 'y': 8}
+        {'x': 3, 'y': 10}
       ],
       "snakes": [
         {
@@ -17,20 +17,19 @@ let request = {
           "name": "Sneky Snek",
           "health": 90,
           "body": [
-            {"x": 10, "y": 0}, {'x': 9, 'y':0}, {"x": 8, "y": 0}, {'x': 7, 'y':0}, {'x': 6, 'y':0}, {'x': 5, 'y':0}, {'x': 4, 'y':0}, {'x': 3, 'y':0}, {'x': 3, 'y':1}, {'x': 3, 'y':2}, {'x': 3, 'y':3}, {'x': 3, 'y':4}, {'x': 2, 'y':4}, {'x': 1, 'y':4}, {'x': 0, 'y':4}, {'x': 0, 'y':5}, {'x': 0, 'y':6}, {'x': 0, 'y':7}, {'x': 0, 'y':8}
-
+            {'x': 10, 'y': 0}, {'x': 10, 'y': 1}
           ],
           "shout": "Hello my name is Sneky Snek"
         },
-        // {
-        //   "id": "snake-id-string",
-        //   "name": "Sneky Snek",
-        //   "health": 90,
-        //   "body": [
-        //     {'x':8 , 'y': 8}, {'x':7,'y':8}
-        //   ],
-        //   "shout": "Hello my name is Sneky Snek"
-        // },
+        {
+          "id": "snake-id-string",
+          "name": "Sneky Snek",
+          "health": 90,
+          "body": [
+            {'x': 8, 'y': 0}, {'x': 8, 'y': 1}, {'x': 8, 'y': 2}
+          ],
+          "shout": "Hello my name is Sneky Snek"
+        },
       ]
     },
     "you": {
@@ -39,7 +38,7 @@ let request = {
       "health": 90,
       "body": [
         // {"x": 10, "y": 1}, {"x": 10, "y": 2}, {"x": 9, "y": 2}
-        {"x": 10, "y": 0}, {'x': 9, 'y':0}, {"x": 8, "y": 0}, {'x': 7, 'y':0}, {'x': 6, 'y':0}, {'x': 5, 'y':0}, {'x': 4, 'y':0}, {'x': 3, 'y':0}, {'x': 3, 'y':1}, {'x': 3, 'y':2}, {'x': 3, 'y':3}, {'x': 3, 'y':4}, {'x': 2, 'y':4}, {'x': 1, 'y':4}, {'x': 0, 'y':4}, {'x': 0, 'y':5}, {'x': 0, 'y':6}, {'x': 0, 'y':7}, {'x': 0, 'y':8}
+        {'x': 10, 'y': 0}, {'x': 10, 'y': 1}
       ],
       "shout": "Hello my name is Sneky Snek"
     }
